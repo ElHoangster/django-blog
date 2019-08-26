b355ae03ff0bd4926a998974e0d50bb4d3d009ea
Brian Hoang <brian@brians-mbp.attlocal.net>
2019-08-25 20:08:48 -0700

    working on comment feature

--- blog/entries/models.py
+++ blog/entries/models.py
@@ -10,6 +10,20 @@ class Entry(models.Model):
 
 	class Meta:
 		verbose_name_plural = "entries"
-
+        
 	def __str__(self):
 		return f'{self.entry_title}'
+
+class Comment(models.Model):
+    entry = models.ForeignKey('entries.Entry', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomeView, EntryView, CreateEntryView
+from .views import HomeView, EntryView, CreateEntryView, CreateCommentView
 
 urlpatterns = [
 	path('', HomeView.as_view(), name = 'blog-home'),
 	path('entry/<int:pk>/', EntryView.as_view(), name = 'entry-detail'),
-	path('create_entry/', CreateEntryView.as_view(success_url='/'), name = 'create_entry')
+	path('create_entry/', CreateEntryView.as_view(success_url='/'), name = 'create_entry'),
+	path('entry/<int:pk>/create_comment/', CreateCommentView.as_view(success_url='entry/<int:pk>/'), name = 'create_comment')
 ]
